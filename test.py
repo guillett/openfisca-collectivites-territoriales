dd8b271ab2a397ae40bb08d05f1b651691061bbf
Thomas Guillet <guillet.thomas@gmail.com>
2020-01-24 22:43:51 +0100

    Compute DGF Population 99.89%

--- test.py
+++ test.py
@@ -2,18 +2,21 @@ import pandas
 from openfisca_collectivites_territoriales import CountryTaxBenefitSystem
 
 
-mapping_path = '/home/thomas/repos/openfisca-collectivites-territoriales/mapping.xlsx'
+mapping_path = 'data/mapping.xlsx'
 mapping_data = pandas.read_excel(mapping_path)
 mapping = { row.OpenFisca: row.Base for row in mapping_data.itertuples() }
 
 tax_benefit_system = CountryTaxBenefitSystem()
 
-file_path_2019 = '/home/thomas/Documents/Beta.gouv.fr/Datafin/2019-communes-criteres-repartition.csv'
+file_path_2019 = 'data/2019-communes-criteres-repartition.csv'
 data_2019 = pandas.read_csv(file_path_2019, low_memory=False)
 length = len(data_2019)
 
-# file_path_2018 = '/home/thomas/Documents/Beta.gouv.fr/Datafin/2018-communes-criteres-repartition.csv'
-# data_2018 = pandas.read_csv(file_path_2018, low_memory=False)
+file_path_2018 = 'data/2018-communes-criteres-repartition.csv'
+data_2018 = pandas.read_csv(file_path_2018, low_memory=False)
+
+data = data_2019.set_index(mapping['depcom']).join(data_2018.set_index(mapping['depcom']), lsuffix='2019', rsuffix='2018')
+# data = data_2019.set_index(mapping['depcom']).join(data_2018.set_index(mapping['depcom']), on=mapping['depcom'], lsuffix="2019", rsuffix="2018")
 
 from openfisca_core.simulation_builder import SimulationBuilder
 import numpy
@@ -23,12 +26,12 @@ simulation = SimulationBuilder().build_default_simulation(tax_benefit_system, le
 period = '2019'
 inputs = ['population_insee', 'nb_residences_secondaires', 'nb_caravanes']
 for i in inputs:
-  simulation.set_input(i, period, numpy.array(data_2019[mapping[i]]))
+  simulation.set_input(i, period, numpy.array(data[mapping[i] + period]))
 
 period = '2018'
 inputs = ['dotation_solidarite_urbaine', 'premiere_fraction_dotation_solidarite_rurale']
 for i in inputs:
-  simulation.set_input(i, period, numpy.array(data_2019[mapping[i]]))
+  simulation.set_input(i, period, numpy.array(data[mapping[i] + period]))
 
 
 period = '2019'
@@ -39,6 +42,5 @@ ko = sum(result)
 print(ko)
 print((length - ko)/ length)
 
-# with pandas.ExcelWriter('error2.xlsx') as writer:
-#   data_2019[result].to_excel(writer, sheet_name='error')
-#   
+with pandas.ExcelWriter('error.xlsx') as writer:
+  data_2019[result].to_excel(writer, sheet_name='error')
